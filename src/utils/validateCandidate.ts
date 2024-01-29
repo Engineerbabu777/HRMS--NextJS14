b111388a9da56663d257544868216de42f4b8e72
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-29 19:17:48 +0500

    added form validations

--- src/utils/validateCandidate.ts
+++ src/utils/validateCandidate.ts
@@ -3,15 +3,17 @@
 import * as yup from 'yup';
 
 
+
+
 // VALIDATION SCHEMA FOR CANDIDATE!
 export const validationSchemaForCandidate = yup.object({
-    name: yup.string().trim().required('Name is required'),
-    email: yup
-      .string()
-      .email('Must be a valid email')
-      .required('Email is required'),
-    imageURL: yup.number().min(1).required('HeadCount is required'),
-    location: yup.string().required('Location is required'),
-    description: yup.string().trim().required('Description is required'),
-    phoneNumber: yup.number().required('PhoneNumber is required')
-  })
+  name: yup.string().trim().required('Name is required'),
+  email: yup
+    .string()
+    .email('Must be a valid email')
+    .required('Email is required'),
+  imageURL: yup.number().min(1).required('HeadCount is required'),
+  location: yup.string().required('Location is required'),
+  description: yup.string().trim().required('Description is required'),
+  phoneNumber: yup.number().required('PhoneNumber is required')
+})
